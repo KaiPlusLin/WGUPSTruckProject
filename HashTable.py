84a33c8235f44ccbf567d0fb2936a4d4abb3e54b
Kaitlin Carleton <kcarle6@wgu.edu>
2024-09-03 14:29:08 -0700

    Add WGUPSDistanceTable.csv and import as dictionary. Move into csv_file directory

--- HashTable.py
+++ HashTable.py
@@ -1,7 +1,7 @@
 import csv
 
 #import csv
-with open('WGUPS.csv', newline='') as csvfile:
+with open('csv_files/WGUPS.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     for row in data:
         print(row)
